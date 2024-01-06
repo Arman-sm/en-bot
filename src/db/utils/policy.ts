0e4d5c9519d1dff35a89fe0e0bd3697c92bff64d
Arman-sm <arman.s.m@outlook.com>
2024-01-06 16:06:17 +0330

    Added checks for the 'allowed_languages' field

--- src/db/utils/policy.ts
+++ src/db/utils/policy.ts
@@ -1,17 +1,28 @@
-import { ZPolicy, type IPolicy } from "../../interfaces/Job"
+import { ZPolicy, type IPolicy, ZLanguagePolicy } from "../../interfaces/Job"
 import { GuildPolicies } from "../repos"
 
 export async function get_guild_policy(guild_id: string): Promise<Partial<IPolicy>> {
-	const res = await GuildPolicies.fetch(guild_id)
-	const zodRes = ZPolicy.partial().safeParse(res)
+	const {allowed_languages, ...res} = await GuildPolicies.fetch(guild_id)
+	
+	const langZodRes = ZLanguagePolicy.partial().safeParse(allowed_languages || {})
+	const mainZodRes = ZPolicy.partial().safeParse(res)
 
-	if (!zodRes.success) { console.error(`Guild ID ${guild_id}'s had invalid data (zod error: ${zodRes.error})`) }
+	//@ts-ignore
+	const err = mainZodRes?.error || langZodRes?.error
+	if (err) {
+		console.error(`Guild ID ${guild_id}'s had invalid data (zod error: ${err})`)
+		return {}
+	}
 
-	return zodRes.success ? zodRes.data : {}
+	//@ts-ignore
+	return {...mainZodRes.data, allowed_languages: langZodRes.data}
 }
 
-export async function set_guild_policy(guild_id: string, value: Partial<IPolicy>) {
-	await GuildPolicies.save(guild_id, ZPolicy.partial().parse(value))
+export async function set_guild_policy(guild_id: string, _value: Partial<IPolicy>) {
+	const {allowed_languages: _allowed_languages, ...value} = _value
+	const allowed_languages = ZLanguagePolicy.partial().parse(_allowed_languages || {})
+
+	await GuildPolicies.save(guild_id, {...ZPolicy.partial().parse(value), allowed_languages})
 }
 
 export async function update_guild_policy(guild_id: string, value: Partial<IPolicy>) {
